125fdcc5a3e418bb1eec3eba3ca7d2e05d1ffc9e
ze-e <white.noise616@gmail.com>
2020-06-22 21:07:48 -0700

    fix: renamed modifier classes to better reflect BEM style

--- scripts/close-button.js
+++ scripts/close-button.js
@@ -2,10 +2,10 @@ let closeButton = document.querySelector('.popup__close');
   
   closeButton.addEventListener('click', () =>{
     let popup = document.querySelector('.popup');
-    popup.classList.remove('popup_opened');
+    popup.classList.remove('popup_state_opened');
 
     //fixme: added an unopened class, because display:none is overwriting diplay:flex in popup_opened
-    popup.classList.add('popup_unopened');
+    popup.classList.add('popup_state_unopened');
 
     console.log(`${popup.classList}`);
     }
--- scripts/edit-button.js
+++ scripts/edit-button.js
@@ -2,9 +2,9 @@ let editButton = document.querySelector('.profile__edit-button');
   
   editButton.addEventListener('click', () => {
     let popup = document.querySelector('.popup');
-    popup.classList.toggle('popup_opened');
+    popup.classList.toggle('popup_state_opened');
     //fixme: added an unopened class, because display:none is overwriting diplay:flex in popup_opened
-    popup.classList.toggle('popup_unopened');
+    popup.classList.toggle('popup_state_unopened');
 
     console.log(`${popup.classList}`);
     }
