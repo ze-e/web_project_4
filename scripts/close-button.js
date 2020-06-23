317a657ed4ff4e191dd15b3a2c43ac0f83ba0dff
ze-e <white.noise616@gmail.com>
2020-06-22 17:39:14 -0700

    added unopened comment in js

--- scripts/close-button.js
+++ scripts/close-button.js
@@ -3,6 +3,8 @@ let closeButton = document.querySelector('.popup__close');
   closeButton.addEventListener('click', () =>{
     let popup = document.querySelector('.popup');
     popup.classList.remove('popup_opened');
+
+    //fixme: added an unopened class, because display:none is overwriting diplay:flex in popup_opened
     popup.classList.add('popup_unopened');
 
     console.log(`${popup.classList}`);
--- scripts/edit-button.js
+++ scripts/edit-button.js
@@ -3,6 +3,7 @@ let editButton = document.querySelector('.profile__edit-button');
   editButton.addEventListener('click', () => {
     let popup = document.querySelector('.popup');
     popup.classList.toggle('popup_opened');
+    //fixme: added an unopened class, because display:none is overwriting diplay:flex in popup_opened
     popup.classList.toggle('popup_unopened');
 
     console.log(`${popup.classList}`);
