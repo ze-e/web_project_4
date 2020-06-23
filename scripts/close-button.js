c7381240d8cf8d2ec349ec770f56f3f35c5a66b9
ze-e <white.noise616@gmail.com>
2020-06-23 12:42:02 -0700

    fixed script for modal window

--- scripts/close-button.js
+++ scripts/close-button.js
@@ -4,9 +4,6 @@ let closeButton = document.querySelector('.popup__close');
     let popup = document.querySelector('.popup');
     popup.classList.remove('popup_state_opened');
 
-    //fixme: added an unopened class, because display:none is overwriting diplay:flex in popup_opened
-    popup.classList.add('popup_state_unopened');
-
     console.log(`${popup.classList}`);
     }
   );
--- scripts/edit-button.js
+++ scripts/edit-button.js
@@ -3,8 +3,6 @@ let editButton = document.querySelector('.profile__edit-button');
   editButton.addEventListener('click', () => {
     let popup = document.querySelector('.popup');
     popup.classList.toggle('popup_state_opened');
-    //fixme: added an unopened class, because display:none is overwriting diplay:flex in popup_opened
-    popup.classList.toggle('popup_state_unopened');
 
     console.log(`${popup.classList}`);
     }
