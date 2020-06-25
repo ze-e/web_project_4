479ebb695e96d89803263317fb2552f839a7f30c
ze-e <white.noise616@gmail.com>
2020-06-25 11:19:08 -0700

    refactor: optimized script for performance by declaring DOM variables at top of file

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,39 @@
+/* asign DOM elements */
+//assigning all DOM elements at the beginning optimizes performance
+
+//popup handler
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close');
+
+//form handler
+const form = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input-name');
+const descriptionInput = document.querySelector('.popup__input-description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+
+
+
 /* popup handler */
 
 function popupHandler(evt){
-    evt.preventDefault();
-
-    const popup = document.querySelector('.popup');
+    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
     popup.classList.toggle('popup_state_opened');
 }
 
 //add handler to edit button and close button
-const editButton = document.querySelector('.profile__edit-button');
 editButton.addEventListener('click', popupHandler);
-
-const closeButton = document.querySelector('.popup__close');
 closeButton.addEventListener('click', popupHandler);
 
 
-/*   submit handler  */
-
-const form = document.querySelector('.popup__form');
-
+/*   form handler  */
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-
-
-    // Let's find the form fields in the DOM
-    const nameInput = form.querySelector('.popup__input-name');
-    const descriptionInput = form.querySelector('.popup__input-description');
-
+    evt.preventDefault(); 
 
     // Get the values of each field from the corresponding value property
     const name = nameInput.value;
     const description = descriptionInput.value;
-    // Select elements where the field values will be entered
-    const profileName = document.querySelector('.profile__name');
-    const profileDescription = document.querySelector('.profile__description');
-
 
     // Insert new values using the textContent property of the querySelector() method
     profileName.textContent = name;
