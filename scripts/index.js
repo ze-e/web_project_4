0d7d60afc8049f11c2b273b47158887cedcab6b3
ze-e <white.noise616@gmail.com>
2020-07-02 17:15:24 -0700

    feat:added add-card form

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,49 @@
 //assigning all DOM elements at the beginning optimizes performance
 
 //popup handler
-const popup = document.querySelector('.popup');
+const popup_editProfile = document.querySelector('.popup_type_edit-profile');
+const popup_addCard = document.querySelector('.popup_type_add-card');
+
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close');
+const addButton = document.querySelector('.profile__add-button');
 
-//form handler
-const form = document.querySelector('.popup__form');
+
+//form handlers
+const form = document.querySelector('.popup__form_type_edit-profile');
 const nameInput = document.querySelector('.popup__input-name');
 const descriptionInput = document.querySelector('.popup__input-description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
+const formAddCard = document.querySelector('.popup__form_type_add-card');
+const cardNameInput = document.querySelector('.popup__input-card-name');
+const cardDescriptionInput = document.querySelector('.popup__input-card-description');
 
 
-/* popup handler */
+/* popup handlers */
 
 function popupHandler(evt){
+
     evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-    popup.classList.toggle('popup_state_opened');
+    popup_editProfile.classList.toggle('popup_state_opened');
+
 }
 
-//add handler to edit button and close button
+function addCardHandler(evt){
+
+    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
+    popup_addCard.classList.toggle('popup_state_opened');
+
+}
+
+//add handlers to buttons
 editButton.addEventListener('click', popupHandler);
 closeButton.addEventListener('click', popupHandler);
+addButton.addEventListener('click', addCardHandler);
 
 
-/*   form handler  */
+/*   form handlers  */
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
 
@@ -41,8 +58,26 @@ function formSubmitHandler (evt) {
 
     //close window after changing values
     popupHandler(evt);
+
+}
+
+function addCardSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    // Get the values of each field from the corresponding value property
+    const name = cardNameInput.value;
+    const description = cardNameInput.value;
+
+    // Insert new values using the textContent property of the querySelector() method
+    //profileName.textContent = name;
+    //profileDescription.textContent = description;
+
+    //close window after changing values
+    addCardHandler(evt);
+
 }
 
 // Connect the handler to the form:
 // it will watch the submit event
 form.addEventListener('submit', formSubmitHandler);
+formAddCard.addEventListener('submit', addCardSubmitHandler);
